d57bf8f888658dbf2683557443cc494033654ed8
arhunt <allan.r.hunt@gmail.com>
2020-10-02 14:10:57 -0500

    Handle change to dropdown.

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,43 @@
 console.log("This is app.js");
 
-function optionChanged(newSubject)
+// **Placeholder for bar graph
+function bargraph(subject)
 {
-    console.log("Subject selected:",newSubject);
+    console.log("Bar graph for:", subject);
+} 
+
+// **Placehlolder for bubble plot
+function bubbplot(subject)
+{
+    console.log("Bubble plot for:", subject)
+}
+
+// **Placeholder for info box
+function infocard(subject)
+{
+    console.log("Info card for:", subject)
+}
+
+// **Placeholder for gauge
+function gaugecht(subject)
+{
+    console.log("Gauge for:", subject)
+}
+
+// Refresh when changing option from dropdown
+function dropchg(newSubject)
+{
+    console.log("Subject selected:", newSubject);
+    bargraph(newSubject);
+    bubbplot(newSubject);
+    infocard(newSubject);
+    gaugecht(newSubject);
 }
 
 function pageload()
 {
-
+    // Grab the selector
     var selector = d3.select("#selDataset");
-    
     // Use the info from the json to load the page
     d3.json("samples.json").then
     ( (data) =>
@@ -31,9 +59,13 @@ function pageload()
         // Initial sample information
         var firstSubject = subjects[0];
         console.log("Starting sample:", firstSubject);
+        // Draw the graphs
+        bargraph(firstSubject);
+        bubbplot(firstSubject);
+        infocard(firstSubject);
+        gaugecht(firstSubject);
     }
     );
-
 }
 
 pageload();
